cc2c39a364c345255bec27f53aa80d2d527b2465
paritosh-py <paritoshpandita@gmail.com>
2025-08-17 16:20:46 +0530

    :New Questions

--- Questions.cpp
+++ Questions.cpp
@@ -1,3 +1,4 @@
+/*
 #include <bits/stdc++.h>
 #include <string>
 using namespace std;
@@ -19,5 +20,55 @@ int main() {
     }              // update the value of n 
     cout << "The solution of the problem is : "<< prod - sum ; 
     
+    return 0;
+}  
+*/
+
+/*
+// Q. Find 1's Compliment of an integer...
+
+#include <bits/stdc++.h>
+#include <string>
+using namespace std;
+
+int main() {
+    
+ int n ;
+ cin >> n ; 
+ int m = n ; 
+ int mask = 0 ; 
+ 
+ while (n != 0)
+// Logic of creating mask in Notes (Questions)
+ {
+    n = n >> 1 ; 
+    mask = (mask << 1) | 1 ;
+     
+ }
+   int ans = ~m & mask ; 
+    cout << ans ; 
+    
+    return 0;
+}
+*/
+
+#include <bits/stdc++.h>
+#include <string>
+using namespace std;
+
+int main() {
+    
+    float n ;
+    cin >> n ;
+    while (n >1 )
+
+    { n = n /2 ; }
+
+    if (n == 1 )
+        {cout <<"True" << endl;} 
+       
+       else
+       {cout << "False";}
+
     return 0;
 }
